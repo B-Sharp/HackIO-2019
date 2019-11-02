f1db29c5c3dc891e9a85e1503008a0bf43bf1b86
SPIGS <paspielvogel@gmail.com>
2019-11-02 16:09:36 -0400

    data extractor

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -13,18 +13,23 @@ public class Map : MonoBehaviour
     void Start()
     {
         this.buildings = new Dictionary<string, Building>();
+        this.getData();       
+    }
 
-        using (var reader = new StreamReader("Assets/Data/HackathonConfig.csv")) {
-            while (!reader.EndOfStream) {
+    private void getData () {
+        using (var reader = new StreamReader("Assets/Data/HackathonConfig.csv"))
+        {
+            while (!reader.EndOfStream)
+            {
                 string line = reader.ReadLine();
                 string[] values = line.Split(',');
                 //get data
                 string name = values[5];
                 name.Trim('\"');
                 string lat_str = values[8];
-                lat_str.Trim('\"');
+                lat_str = lat_str.Trim('\"');
                 string long_str = values[9];
-                long_str.Trim('\"');
+                long_str = long_str.Trim('\"');
                 string campus = values[10];
                 string purpose = values[12];
 
@@ -35,13 +40,24 @@ public class Map : MonoBehaviour
                 // get Resource and Unit
                 //Debug.Log(res_str);
 
-                if (res_str.Equals("\"Heating Hot Water\"")) {
+                if (res_str.Equals("\"Heating Hot Water\""))
+                {
                     resource = Resource.HotWater;
-                    Debug.Log("Hot Water");
-                } else if (res_str.Equals("\"Chilled Water \"")) {
+                }
+                else if (res_str.Equals("\"Chilled Water \""))
+                {
                     resource = Resource.ChilledWater;
-                    Debug.Log("Cold water");
-                } else {
+                }
+                else if (res_str.Equals("\"Steam\""))
+                {
+                    resource = Resource.Steam;
+                }
+                else if (res_str.Equals("\"Electricity\""))
+                {
+                    resource = Resource.Electricity;
+                }
+                else
+                {
                     resource = Resource.Unknown;
                     Debug.Log("Unknow source");
                     Debug.Log("unknown source: " + res_str);
@@ -50,28 +66,33 @@ public class Map : MonoBehaviour
                 // get lat and lon
                 double latitude;
                 double longitude;
-                try {
-                     latitude = double.Parse(lat_str);
-                     longitude = double.Parse(long_str);
-                } catch {
-                    //Debug.Log(lat_str + " " + long_str);
+                try
+                {
+                    latitude = double.Parse(lat_str);
+                    longitude = double.Parse(long_str);
+                }
+                catch
+                {
                     latitude = 0.0;
                     longitude = 0.0;
                 }
-               
 
-                
-                if (!this.buildings.ContainsKey(name)) {
+
+
+                if (!this.buildings.ContainsKey(name))
+                {
                     //put it indictionary
                     Building building = new Building(name, latitude, longitude, resource, purpose);
                     this.buildings.Add(name, building);
-                } else {
+                }
+                else
+                {
                     // add resource info to building entry
                     buildings[name].addResource(resource);
                 }
-                
+
             }
-        }       
+        }
     }
 
     // Update is called once per frame
